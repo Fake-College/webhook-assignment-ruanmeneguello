3566a2def4e2771e5a9cc1707c5e33af06655066
ruanmeneguello <63620885+ruanmeneguello@users.noreply.github.com>
2025-10-01 13:13:07 -0600

    Update comment from grade 2 to grade 3

--- putting_python_to_work.py
+++ putting_python_to_work.py
@@ -1,6 +1,6 @@
 # Get the radius of the circle from the user
 import math
-# testing grade 2
+# testing grade 3
 radius = float(input("Enter the radius of the circle: "))
 
 # Calculate the diameter of the circle
